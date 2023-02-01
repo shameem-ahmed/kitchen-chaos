ad37d7b07d1081d2361628786caebcf2bf1db80f
Shameem Ahmed <shameem.net1@gmail.com>
2023-02-02 03:35:36 +0530

    added Scriptable objects

--- KitchenChaoes/Assets/Scripts/ClearCounter.cs
+++ KitchenChaoes/Assets/Scripts/ClearCounter.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform topPoint;
+
+    [SerializeField] private ClearCounter secondClearCounter;
+
+
+    private KitchenObject kitchenObject;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
 
     public void Interact()
     {
-        Transform tomatoTransform = Instantiate(tomatoPrefab, topPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, topPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return topPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() 
+    { 
+        return kitchenObject; 
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
